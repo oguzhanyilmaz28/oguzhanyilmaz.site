b77ae49f3620043095a266e261de08d5a36049c0
Oğuzhan Yılmaz <101720462+oguzhanyilmaz28@users.noreply.github.com>
2024-04-17 19:43:45 +0300

    Update experiences-timeline.jsx

--- components/experiences-timeline.jsx
+++ components/experiences-timeline.jsx
@@ -8,26 +8,20 @@ export default function ExperiencesTimeline() {
                 <li className="px-2">
                     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                     <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                        Jun 2023 - Sep 2023 | 3 month
+                        October 2021 - Sep 2022 | 11 month
                     </time>
 
                     <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                        <Link
-                            href="https://www.unizar.es/"
-                            target="_blank"
-                            className="text-lg font-semibold text-gray-900 dark:text-white hover:text-blue-600 dark:hover:text-blue-600"
-                        >
                             Erasmus+ Internship
-                        </Link>
                     </h3>
                     <h5 className="font-medium text-base text-gray-700 dark:text-gray-300">
-                        Universidad de Zaragoza
+                        Volunteer Software Engineer
                     </h5>
                     <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        I did my internship in Zaragoza, Spain. I successfully completed the task of creating a dynamic website for the company I worked for. I also gained experience in the field of web development.
+                        I implemented various artificial intelligence projects with Python & Java.
                     </p>
                 </li>
-                <li className="mb-4 px-2">
+               {/*  <li className="mb-4 px-2">
                     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                     <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                         November 2021 - Mar 2022 | 5 month
@@ -47,7 +41,7 @@ export default function ExperiencesTimeline() {
                     <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                         Completed a valuable IT internship at Balikesir University, gaining hands-on experience in supporting and maintaining IT infrastructure while contributing effectively to the universitys technology initiatives.
                     </p>
-                </li>
+                </li> */}
             </ol>
         </div>
     );
