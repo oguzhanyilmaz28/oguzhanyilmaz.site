77dc46c873586135abcfeb15f6cad7aa6d1c3945
Oğuzhan Yılmaz <101720462+oguzhanyilmaz28@users.noreply.github.com>
2025-02-18 00:05:01 +0300

    Update about.jsx

--- layouts/about.jsx
+++ layouts/about.jsx
@@ -8,12 +8,12 @@ export default function About() {
                 About <span className="text-primary">ME</span>
             </div>
             <p className="text-gray-700 antialiased text-xl font-light max-w-xl tracking-wider leading-8 dark:text-slate-50 px-2">
-            I am a 3rd year computer engineering student at Balikesir University.
+            I am a 4rd year computer engineering student at Balikesir University.
             I have an investigative and determined personality.
             I always try to improve myself and learn new things in the field of software.
             I have an intermediate level English.I am interested in back-end and front-end development.
             I have good knowledge of intermediate C#,Python and SQL.
-            In additipn,I develop projects using C# ,.NET Core,Html,Css,JavaScript,Php and Python.
+            In additipn,I develop projects using C# ,.NET Core,Html,Css,JavaScript,React,Php,Flutter and Python.
             </p>
             <div className="gap-3 flex flex-col sm:flex-row">
                 <ViewResumeBtn />
